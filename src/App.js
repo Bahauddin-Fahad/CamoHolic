93f11cf4744d50c9f8a8be024ad9bfcde513a2c8
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-03-26 23:26:37 +0600

    final commit"

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import Header from "./component/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Shop from "./component/Shop/Shop";
 import QandA from "./component/QandA/QandA";
-<script
-  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-  crossorigin
-></script>;
 function App() {
   return (
     <div className="App">
